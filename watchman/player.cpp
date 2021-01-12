74d577ae0c42e2b4cbf567e1eee4a4b36bc25225
Johan Mora <johan.morar@udea.edu.co>
2021-01-12 02:04:07 -0500

    Correccion de errores de movimmiento, inclusion de puntaje y vida del personaje

---
+++ watchman/player.cpp
@@ -0,0 +1,96 @@
+#include "player.h"
+#include "game.h"
+
+extern Game * juego;
+
+Player::Player(QGraphicsItem * parent)
+{
+    timer = new QTimer();
+    fila = 5;
+    columna =5;
+    pixmap = new QPixmap(":/recursos/imagenes/Sprit Rick.png");
+    setScale(0.5);
+    //dimensiones de cada imagen
+    ancho=125;
+    alto=160;
+
+    //Timer_restar = new QTimer();
+    //connect(Timer_restar,SIGNAL(timeout()),this,SLOT(Restar_Vida()));
+    //Timer_restar->start(100);
+}
+
+void Player::keyPressEvent(QKeyEvent *event)
+{
+
+    if(event->key()==Qt::Key_A){
+        if(pos().x() > 0){
+            setPos(x()-10,y());
+            Movimiento();
+            fila=340;
+        }
+    }
+
+    else if(event->key()==Qt::Key_D){
+        if(pos().x() + 50 < 2000){
+            setPos(x()+10,y());
+            Movimiento();
+            fila=5;
+        }
+    }
+
+    else if(event->key()==Qt::Key_W){
+        if(pos().y()>0){
+            setPos(x(),y()-10);
+            Movimiento();
+            fila=505;
+        }
+    }
+
+    else if(event->key()==Qt::Key_S){
+        if(pos().y()+50 < 1200){
+            setPos(x(),y()+10);
+            Movimiento();
+            fila=172;
+        }
+    }
+
+    else if(event->key()==Qt::Key_Space){
+        Poder *power =new Poder(fila);
+        power->setPos(x()-20,y());
+        scene()->addItem(power);
+
+    }
+}
+
+void Player::Movimiento()
+{
+    columna +=130;
+    if(columna>=520){
+        columna=5;
+    }
+    this->update(-ancho/2,-alto/2,ancho,alto);
+
+}
+/*
+void Player::Restar_Vida()
+{
+    //qDebug() << "shit";
+    QList<QGraphicsItem * >collinding_Items = collidingItems();
+    for(int i = 0; i < collinding_Items.size();i++){
+        if(typeid (*(collinding_Items[i]))==typeid (Enemy)){
+            qDebug() <<-1;
+        }
+    }
+}
+
+*/
+
+QRectF Player::boundingRect() const
+{
+    return QRectF(-ancho/2,-alto/2,ancho,alto);
+}
+
+void Player::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawPixmap(-ancho/2,-alto/2,*pixmap,columna,fila,ancho,alto);
+}
---
+++ watchman/player.h
@@ -0,0 +1,37 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <QMediaPlayer>
+#include <QTimer>
+#include <QPainter>
+#include <QDebug>
+#include "poder.h"
+
+class Player: public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    Player(QGraphicsItem * parent = 0);
+    void keyPressEvent(QKeyEvent *event);
+    void Movimiento();
+
+    QTimer *timer;
+    QPixmap *pixmap;
+
+    QTimer *Timer_restar;
+
+    float fila,columna;
+    float ancho,alto;
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+
+public slots:
+    //void Restar_Vida();
+
+};
+
+#endif // PLAYER_H
